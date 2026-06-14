0082ce525213dc68d6eefc6f3570b9bc47869773
Phong205 <phongqte@gmail.com>
2026-06-14 12:47:42 +0700

    Tạo chức năng đăng nhập cho hệ thống. Chỉnh sửa hệ thống theo mô hình MVC

---
+++ app/src/main/java/com/example/quanlydeadline/database/UserDao.java
@@ -0,0 +1,22 @@
+package com.example.quanlydeadline.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import com.example.quanlydeadline.models.User;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    void registerUser(User user);
+
+    @Query("SELECT * FROM users WHERE email = :email AND password_hash = :password LIMIT 1")
+    User login(String email, String password);
+
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    User getUserByEmail(String email);
+
+    @Insert
+    long insertUser(User user);
+}
